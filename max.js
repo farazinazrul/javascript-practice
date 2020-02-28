9c56e0a04306fc26552de5942aceb1f8c65fb77a
farazinazrul <farazinazrul@yahoo.com>
2020-02-28 22:08:29 +0600

    comparison

---
+++ max.js
@@ -0,0 +1,12 @@
+var business = 4000;
+var minister = 35000;
+var shochib = 530000;
+
+if (business > minister) {
+    console.log("Business")
+} else if(minister > shochib) {
+    console.log("Minister")
+}
+else{
+    console.log("Shochib")
+}

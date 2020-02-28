c9112cc88f9fe932d2fb448fab686991e3287fb4
farazinazrul <farazinazrul@yahoo.com>
2020-02-28 12:25:17 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,9 @@
+var priceOfGoods= 12;
+console.log(priceOfGoods)
+var mind = "good";
+var pocketerTaka = "more or less";
+var position = "1-10";
+console.log(mind);
+console.log(pocketerTaka)
+console.log(position)
+

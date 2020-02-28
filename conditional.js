182c74d29b9a82b3f21d608cdab03aafce4bad88
farazinazrul <farazinazrul@yahoo.com>
2020-02-28 12:12:59 +0600

    first commit

---
+++ conditional.js
@@ -0,0 +1,7 @@
+var dimerDam = 12;
+
+if (dimerDam<10) {
+console.log("Dim Kinbo")
+} else {
+console.log("Huda Vat")    
+}
---
+++ hello.js
@@ -0,0 +1,9 @@
+var age=35;
+console.log(age);
+var weather="hot"
+var me = "man"
+console.log(me)
+console.log(weather)
+var WhatIsDate = 26;
+console.log(WhatIsDate);
+
